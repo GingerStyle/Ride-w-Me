9aca73f7af1bfabeab309d727e04cebea65e5a32
Myles Voigt <myles.voigt@gmail.com>
2023-02-25 14:30:48 -0600

    setup server to use port 5001 instead of 5000, created .env file, setup tables in the database.sql file

--- database.sql
+++ database.sql
@@ -6,5 +6,29 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "phone" VARCHAR(11)
+);
+
+CREATE TABLE "bike" (
+    "id" SERIAL PRIMARY KEY,
+    "type" VARCHAR
+);
+
+CREATE TABLE "dates" (
+    "id" SERIAL PRIMARY KEY,
+    "dates" DATE
+);
+
+CREATE TABLE "user_bike" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user" NOT NULL,
+    "bike_id" INT REFERENCES "bike" NOT NULL
+);
+
+CREATE TABLE "user_dates" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user" NOT NULL,
+    "dates_id" INT REFERENCES "dates" NOT NULL
 );
